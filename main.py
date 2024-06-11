02c707cd31f5b86bbb510f1511483bb87ee112fa
Olgierd <olgierdkrzyzaniak@gmail.com>
2024-06-11 19:16:21 +0200

    Revert "Add function to get display name using ctypes"
    This reverts commit 6c46e49f23960c523d7a63db413ed42ba8553363.

--- main.py
+++ main.py
@@ -3,20 +3,6 @@ from tkinter import ttk
 from datetime import datetime
 import openpyxl as xl
 import getpass
-import ctypes
-
-
-def get_display_name():
-    GetUserNameEx = ctypes.windll.secur32.GetUserNameExW
-    NameDisplay = 3
-
-    size = ctypes.pointer(ctypes.c_ulong(0))
-    GetUserNameEx(NameDisplay, None, size)
-
-    nameBuffer = ctypes.create_unicode_buffer(size.contents.value)
-    GetUserNameEx(NameDisplay, nameBuffer, size)
-    return nameBuffer.value
-
 
 CONSTANTS = {"take product": "1", "return product": "2", "cancel": "3"}
 
@@ -79,7 +65,7 @@ class InventoryApp:
         self.root.title("VOESTALPINE Inventory Management System")
 
         self.database = database
-        self.current_user = get_display_name()
+        self.current_user = getpass.getuser()
         print(self.current_user)
         self.record_id = 1
 
